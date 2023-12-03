14103375b24a5b002eed80d06bb7755abdac0648
Wei.Wang <1849553578@qq.com>
2023-12-03 17:08:48 +0800

    feat>单片机>STM32 代码编译成功

--- 000.STM32/000.STM32F103C8T6/000.STM32F103C8T6_1/000.STM32F103C8T6_1/src/main.c
+++ 000.STM32/000.STM32F103C8T6/000.STM32F103C8T6_1/000.STM32F103C8T6_1/src/main.c
@@ -1,11 +1,9 @@
 #include "stm32f10x.h"
-#include "stm32f10x_rcc.h"
 
 int main(int argc, char **argv)
 {
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
-
-    while (1)
+    for (int index = 0; index < 10; index++)
     {
     }
 

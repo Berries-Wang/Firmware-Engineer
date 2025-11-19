130758e1a9742f99d6a05e8a628313ebfbb1b356
Berries.Wang <1849553578@qq.com>
2025-11-19 20:37:16 +0800

    STM32 USART:串口通信

--- 000.STM32/000.STM32F103C8T6/000.STM32F103C8T6_1/015.STM32_UART/src/main.c
+++ 000.STM32/000.STM32F103C8T6/000.STM32F103C8T6_1/015.STM32_UART/src/main.c
@@ -1,23 +1,11 @@
 #include "stm32f10x.h"
+#include "stm32f10x_usart.h"
 
+/**
+ * 同putty相互传输数据
+ */
 int main(int argc, char **argv)
 {
-
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
-
-     
-        GPIO_InitTypeDef gpioInitConfigPB = {
-            .GPIO_Pin = GPIO_Pin_12,
-            .GPIO_Speed = GPIO_Speed_50MHz,
-            .GPIO_Mode = GPIO_Mode_Out_PP,
-        };
-        GPIO_Init(GPIOB, &gpioInitConfigPB);
-    
-
-    GPIO_SetBits(GPIOB, GPIO_Pin_12);
-
-    for (;;)
-        ;
 
     return 0;
 }
---
+++ 000.STM32/012.UART/README.md
@@ -0,0 +1,2 @@
+# UART <sup>Universal synchronous asynchronous receiver
+transmitter.通用同步异步收发传输器</sup>
